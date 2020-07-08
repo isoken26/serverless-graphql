2507dff90850cc0a4936b686ce9e038f60ba720e
kenjiro.isomura (JG20035) <kenjiro.isomura@linecorp.com>
2020-07-08 19:36:37 +0900

    some fix

--- functions/api.js
+++ functions/api.js
@@ -22,11 +22,6 @@ const schema = new GraphQLSchema({
 const app = express();
 
 app.use(express.json());
-app.use('/.netlify/functions/api',
- graphqlHTTP({
-  schema: schema,
-  graphiql: true,
-}))
 app.use(
   "/",
   graphqlHTTP({
